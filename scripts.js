32852a6ae93ecc0bf29ec82fc9a5e080c0a6951b
Franz <franzj.pretorius.2003@gmail.com>
2024-05-23 14:56:36 +0200

    completed the first exercises and added html

--- scripts.js
+++ scripts.js
@@ -13,3 +13,55 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+
+//EXERCISES
+names.forEach(name => console.log(name))
+provinces.forEach(province => console.log(province))
+
+names.forEach((name, index) => {
+  let string = `${name} (${provinces[index]})`
+  console.log(string)
+})
+
+
+const upperProvinces = provinces.map(province => province.toUpperCase())
+console.log(upperProvinces)
+
+
+const lengthNames = names.map(name => name.length)
+console.log(lengthNames)
+
+
+console.log(provinces.sort())
+
+
+const noCapeProvinces = provinces.filter(province => {
+  if(!province.includes('Cape')) {
+    return province
+  }
+})
+console.log(noCapeProvinces.length)
+
+
+let indexOfS = -1
+while(names.some((name, index) => {
+  if(name.includes('S') || name.includes('s')){
+    indexOfS = index
+    return name
+  }
+})) {
+  names[indexOfS] = ''
+}
+
+const namesWithS = names.map(name => {
+  if(name === '') {
+    return true
+  }else{
+    return false
+  }
+})
+console.log(namesWithS)
+
+
+//ADVANCED EXERCISES
