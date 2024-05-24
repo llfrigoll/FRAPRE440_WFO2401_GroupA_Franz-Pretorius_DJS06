b2d042ce8b1a2ffd665c4fb0533ed18f04752f11
Franz <franzj.pretorius.2003@gmail.com>
2024-05-24 08:53:34 +0200

    Added comments

--- scripts.js
+++ scripts.js
@@ -16,31 +16,33 @@ const products = [
 
 
 //EXERCISES
+//console.logs all names with a corresponding province
 names.forEach((name, index) => {
   let string = `${name} (${provinces[index]})`
   console.log(string)
 })
 
 
+//console logs the provinces in uppercase
 const upperProvinces = provinces.map(province => province.toUpperCase())
 console.log(upperProvinces)
 
 
+//console logs the length of the names in the array
 const lengthNames = names.map(name => name.length)
 console.log(lengthNames)
 
 
+//console logs the provinces sorted alphabetically
 console.log(provinces.sort())
 
 
-const noCapeProvinces = provinces.filter(province => {
-  if(!province.includes('Cape')) {
-    return province
-  }
-})
+//filters out the provinces containing 'Cape' and console logs the length of the array
+const noCapeProvinces = provinces.filter(province => !province.includes('Cape'))
 console.log(noCapeProvinces.length)
 
 
+//uses some to check if there is a value containing an s and if so the while loop will run and blank out the name
 let indexOfS = -1
 while(names.some((name, index) => {
   if(name.includes('S') || name.includes('s')){
@@ -51,6 +53,7 @@ while(names.some((name, index) => {
   names[indexOfS] = ''
 }
 
+//uses map to set any blank values to true and any other names to false and console logs the array
 const namesWithS = names.map(name => {
   if(name === '') {
     return true
@@ -62,19 +65,25 @@ console.log(namesWithS)
 
 
 //ADVANCED EXERCISES
+//console logs the product array's products
 console.log(products.map(item => item.product))
 
 
+//filters out the product names that are longer than 5 and console logs it
 console.log(products.filter(item => item.product.length <= 5))
 
 
+//filters out products with blank prices, then converts and string to a number and then uses reduce to add those numbers and console logs it
 console.log(products.filter(item => item.price > 0).map(item => Number(item.price)).reduce((total, num) => total + num))
 
 
+//console logs all the product names in a single line
 console.log(products.reduce((string, item) => string + ' ' + item.product, ''))
 
 
+//uses max and min to get the highest and lowest number in the array and console logs it
 console.log('Highest: ' + Math.max(...products.filter(item => item.price > 0).map(item => Number(item.price))) + '. Lowest: ' + Math.min(...products.filter(item => item.price > 0).map(item => Number(item.price))))
 
 
+//uses map to restructure the item object with new keys and console logs it
 console.log(products.map((item) => {return item = {name: item.product, cost: item.price}}))
